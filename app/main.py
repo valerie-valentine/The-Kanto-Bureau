b67428730207da81440bfdd4c85b1d0e1398fd67
Valerie Valentin <valerie.valentine93@gmail.com>
2025-06-30 20:38:06 -0700

    parsing API response to retrieve only the data I need

--- app/main.py
+++ app/main.py
@@ -6,7 +6,18 @@ def get_pokemon(name):
 
     response = requests.get(url)
 
-    return response.text
+    data = response.json()
+
+    pokemon_summary = {
+        "id": data["id"],
+        "name": data["name"],
+        "sprite": data["sprites"]["front_default"],
+        "types": [t["type"]["name"] for t in data["types"]],
+        "stats": {s["stat"]["name"]: s["base_stat"] for s in data["stats"]},
+        "abilities": [a["ability"]["name"] for a in data["abilities"]]
+    }
+
+    return pokemon_summary
 
 
-print(get_pokemon("clefairy"))
+print(get_pokemon("pikachu"))
