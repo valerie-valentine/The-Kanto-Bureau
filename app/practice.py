1fdc947244ad0e993367a90d22bd8b33861a66cc
Valerie Valentin <valerie.valentine93@gmail.com>
2025-06-30 20:44:37 -0700

    created a practice file to practice retrieving data from third-party api, seperate from main.py file

---
+++ app/practice.py
@@ -0,0 +1,23 @@
+import requests
+
+
+def get_pokemon(name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}/"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    pokemon_summary = {
+        "id": data["id"],
+        "name": data["name"],
+        "sprite": data["sprites"]["front_default"],
+        "types": [t["type"]["name"] for t in data["types"]],
+        "stats": {s["stat"]["name"]: s["base_stat"] for s in data["stats"]},
+        "abilities": [a["ability"]["name"] for a in data["abilities"]]
+    }
+
+    return pokemon_summary
+
+
+print(get_pokemon("charizard"))
