734ff2be4824937dc78edf9f981d3f5216a80047
Valerie Valentin <valerie.valentine93@gmail.com>
2025-07-17 18:24:10 -0700

    created database.py with sqlalchemy & postgres connection

---
+++ app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv
+
+DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+engine = create_engine(DATABASE_URL)
+Session_Local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base
