b6dd0a9dbb82e4497212e10ad6fd587f08a9da39
Valerie Valentin <valerie.valentine93@gmail.com>
2025-07-17 18:30:14 -0700

    fixed typo in database.py

--- app/database.py
+++ app/database.py
@@ -8,5 +8,5 @@ load_dotenv
 DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 engine = create_engine(DATABASE_URL)
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-base = declarative_base
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
